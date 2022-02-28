4324bebccc74569a77816caa45d951fabf1c7035
musleh0001 <khan.muslehuddin0001@gmail.com>
2022-03-01 01:14:05 +0600

    Add mastercard.py file and update README.md

---
+++ ppython/snippets/mastercard.py
@@ -0,0 +1,15 @@
+def setup():
+    noStroke()
+
+    fill(200)
+    rect(100, 100, 300, 150)
+
+    fill(220, 20, 60)
+    ellipse(160, 125, 100, 100)
+
+    fill(255, 215, 0)
+    ellipse(230, 125, 100, 100)
+
+
+def draw():
+    pass
